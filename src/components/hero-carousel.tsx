bf1e659c5904b061032514da8c3c64d8a2a59363
Raizan Shirshikar <Raizanshirshikar2@gmail.com>
2025-12-16 08:47:29 +0000

    can you cahnge this design of carousel make some thing professnal (_for

--- src/components/hero-carousel.tsx
+++ src/components/hero-carousel.tsx
@@ -3,6 +3,8 @@ import {
   Carousel,
   CarouselContent,
   CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
 } from '@/components/ui/carousel';
 import { Button } from '@/components/ui/button';
 import Autoplay from 'embla-carousel-autoplay';
