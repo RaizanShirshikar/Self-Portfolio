c1f4a698bc2e5ee8866b406074f32dd18ecf95ac
Raizan Shirshikar <Raizanshirshikar2@gmail.com>
2025-12-29 09:01:21 +0000

    make less gap from this section and about me section  (_for element <Car

--- src/app/(home)/_components/about.tsx
+++ src/app/(home)/_components/about.tsx
@@ -2,7 +2,7 @@ import { AboutMe } from '@/components/sections/about-me';
 
 export function About() {
   return (
-    <section id="about" className="w-full py-16 md:py-24 lg:py-32">
+    <section id="about" className="w-full py-12 md:py-16 lg:py-20">
       <AboutMe />
     </section>
   );
